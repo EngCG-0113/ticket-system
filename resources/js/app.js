1ff0e5c442e0ddd752510d97a25e0f758cdb5e7b
Eng Chun Guang <engchunguang@gmail.com>
2023-05-14 22:02:20 +0800

    init: auth and blade

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    ExampleComponent
+  }
+});
